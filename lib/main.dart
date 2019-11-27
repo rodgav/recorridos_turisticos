cd04eaf69039c0d7a16b4e1fc0e4cde231987286
Rodolfo Samuel Gavilan Muñoz <30939186+bankenro@users.noreply.github.com>
2019-11-27 12:56:22 -0500

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,18 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:recorridos_turisticos/src/app.dart';
 
-void main() => runApp(App());
+class AppHttpOverrides extends HttpOverrides{
+  @override
+  HttpClient createHttpClient(SecurityContext context){
+    return super.createHttpClient(context)
+      ..badCertificateCallback = (X509Certificate cert, String host, int port)=> true;
+  }
+}
+
+void main(){
+  HttpOverrides.global = AppHttpOverrides();
+  runApp(App());
+}
+
